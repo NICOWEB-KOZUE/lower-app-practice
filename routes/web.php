5e4791440c8abd6db62b14ac9b06c82a4c0290a2
NICOWEB-KOZUE <mew0119@gmail.com>
2025-10-04 21:27:49 +0900

    lower修正

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/lower/{word}', function ($word) {
-    $lower = strtolower($word);
-    return view('lower', [
-        'original' => $word,
-        'lower' => $lower,
-    ]);
-});
-
 Volt::route('/lower/{word}', 'lower');
